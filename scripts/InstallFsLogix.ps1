3e742987796a7e7fcc7b13c943d7978429fff86c
Bryan Long <bryan.m.long@accenture.com>
2022-04-14 13:16:29 -0400

    sdwfewfwef

--- scripts/InstallFsLogix.ps1
+++ scripts/InstallFsLogix.ps1
@@ -1,17 +1,6 @@
 $fsLogixURL="https://aka.ms/fslogix_download"
 $installerFile="fslogix_download.zip"
-$LocalPath = 'C:\\fslogix'
-New-Item -Path C:\\ -Name fslogix -ItemType Directory -ErrorAction SilentlyContinue
-set-Location $LocalPath
-Invoke-WebRequest $fsLogixURL -OutFile $LocalPath\$installerFile
-Expand-Archive $LocalPath\$installerFile -DestinationPath $LocalPath
-
-#########################
-#    FSLogix Install    #
-#########################
-Add-Content -LiteralPath C:\New-WVDSessionHost.log "Installing FSLogix"
-$fslogix_deploy_status = Start-Process `
-    -FilePath "$LocalWVDpath\FSLogix\x64\Release\FSLogixAppsSetup.exe" `
-    -ArgumentList "/install /quiet" `
-    -Wait `
-    -Passthru
+$installPath=New-Item -Path C:\ -Name fslogix -ItemType Directory -ErrorAction SilentlyContinue
+Invoke-WebRequest $fsLogixURL -OutFile $installPath\$installerFile
+Expand-Archive $installPath\$installerFile -DestinationPath $installPath
+Start-Process -FilePath "$installPath\FSLogix\x64\Release\FSLogixAppsSetup.exe" -ArgumentList "/install /quiet" -Wait -Passthru
