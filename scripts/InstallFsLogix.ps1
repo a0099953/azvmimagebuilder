e1e6400d3e8b51f11a58531bd52cc8122b7e0835
Bryan Long <bryan.m.long@accenture.com>
2022-04-18 11:17:11 -0400

    svsdvds

--- scripts/InstallFsLogix.ps1
+++ scripts/InstallFsLogix.ps1
@@ -3,4 +3,4 @@ $installerFile="fslogix_download.zip"
 $installPath=New-Item -Path C:\ -Name fslogix -ItemType Directory -ErrorAction SilentlyContinue
 Invoke-WebRequest $fsLogixURL -OutFile $installPath\$installerFile
 Expand-Archive $installPath\$installerFile -DestinationPath $installPath
-Start-Process -FilePath "$installPath\FSLogix\x64\Release\FSLogixAppsSetup.exe" -ArgumentList "/install /quiet" -Wait -Passthru
+Start-Process -FilePath "C:\FSLogix\x64\Release\FSLogixAppsSetup.exe" -ArgumentList "/install /quiet" -Wait -Passthru
