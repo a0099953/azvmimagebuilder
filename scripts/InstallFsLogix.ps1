639b5bd134fa120f08162284f87a57a2320764dd
a0099953 <56277014+a0099953@users.noreply.github.com>
2022-08-12 11:25:26 -0400

    Update InstallFsLogix.ps1

--- scripts/InstallFsLogix.ps1
+++ scripts/InstallFsLogix.ps1
@@ -1,6 +1,6 @@
 $fsLogixURL="https://aka.ms/fslogix_download"
 $installerFile="fslogix_download.zip"
-$installPath=New-Item -Path C:\ -Name fslogix -ItemType Directory -ErrorAction SilentlyContinue
+$installPath=New-Item -Path "$env:TEMP\FSLogix" -Name fslogix -ItemType Directory -ErrorAction SilentlyContinue
 Invoke-WebRequest $fsLogixURL -OutFile $installPath\$installerFile
 Expand-Archive $installPath\$installerFile -DestinationPath $installPath
-Start-Process -FilePath "C:\FSLogix\x64\Release\FSLogixAppsSetup.exe" -ArgumentList "/install /quiet" -Wait -Passthru
+Start-Process -FilePath "$env:TEMP\FSLogix\x64\Release\FSLogixAppsSetup.exe" -ArgumentList "/install /quiet" -Wait -Passthru
