f82e959f36ca655c8bdcb79faa6cdc1c78c4e4eb
a0099953 <56277014+a0099953@users.noreply.github.com>
2022-08-12 17:28:48 -0400

    Update InstallFsLogix.ps1

--- scripts/InstallFsLogix.ps1
+++ scripts/InstallFsLogix.ps1
@@ -1,6 +1,10 @@
-$fsLogixURL="https://aka.ms/fslogix_download"
-$installerFile="fslogix_download.zip"
-$installPath=New-Item -Path "$env:TEMP\FSLogix" -Name fslogix -ItemType Directory -ErrorAction SilentlyContinue
-Invoke-WebRequest $fsLogixURL -OutFile $installPath\$installerFile
-Expand-Archive $installPath\$installerFile -DestinationPath $installPath
+$fileexists = Test-Path -Path 'c:\windows\tmp\fslogix.txt'
+ If ($fileexists ) {
+    Exit
+}
+New-Item -Path "$env:TEMP\FSLogix" -ItemType Directory -ErrorAction SilentlyContinue
+Copy-Item -Path "\\saambgfilesharescus01.file.core.windows.net\installs\FSLogix_Apps_2.9.8228.50276.zip" -Destination "$env:TEMP\FSLogix" -Recurse
+Expand-Archive "$env:TEMP\FSLogix\FSLogix_Apps_2.9.8228.50276.zip" -DestinationPath "$env:TEMP\FSLogix" -Force
 Start-Process -FilePath "$env:TEMP\FSLogix\x64\Release\FSLogixAppsSetup.exe" -ArgumentList "/install /quiet" -Wait -Passthru
+New-Item -Path "c:\windows\tmp" -ItemType Directory -ErrorAction SilentlyContinue
+New-Item -Path 'c:\windows\tmp\fslogix.txt' -ItemType File -ErrorAction SilentlyContinue
